2aff3b03b4264f0264d4f2ff5522fd3f15a35908
Chris McKinney <chris@tachibanatech.com>
2019-12-11 02:58:12 -0500

    Guest additions and clipboard syncing

--- cookbooks/xubuntu/recipes/default.rb
+++ cookbooks/xubuntu/recipes/default.rb
@@ -1,6 +1,7 @@
 # vim: set ft=ruby ts=2 sts=2 sw=2 et :
 log "Installing xubuntu-desktop takes a long time."
 package "xubuntu-desktop"
+package "virtualbox-guest-x11"
 package "gdebi"
 cookbook_file "/etc/lightdm/lightdm.conf" do
   source "lightdm.conf"
